b750e321eb92973ecdee76176d381739d4795140
Vivekpatnaik013 <102510349+Vivekpatnaik013@users.noreply.github.com>
2024-07-21 20:55:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 
 service_obj=Service()
 deriver= webdriver.Chrome(service=service_obj)
-deriver.maximize_window()
+deriveraximize_window()
 deriver.get("https://www.amazon.in/events/")
 deriver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3F%26ext_vrnc%3Dhi%26tag%3Dgooghydrabk1-21%26ref%3Dnav_custrec_signin%26adgrpid%3D58355126069%26hvpone%3D%26hvptwo%3D%26hvadid%3D610644601173%26hvpos%3D%26hvnetw%3Dg%26hvrand%3D16915603290797267089%26hvqmt%3De%26hvdev%3Dc%26hvdvcmdl%3D%26hvlocint%3D%26hvlocphy%3D9153350%26hvtargid%3Dkwd-10573980%26hydadcr%3D14453_2316415&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
 deriver.back()
